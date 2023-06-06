534b9de5da5e965f20e649b827bd986e554f2270
gam32bit <joseph.caterine@gmail.com>
2023-06-05 20:58:58 -0400

    more detail

--- tdo_tables.py
+++ tdo_tables.py
@@ -8,41 +8,36 @@ import seaborn as sns
 
 pdf = pdfplumber.open("tdostats.pdf")
 
-totals_columns = ["Year", "ECOs", "Crisis_evals", "TDOs", "Per_Total_Evals", "Total_Events",
-                  "Per_Total_TDOs", "Total_LOC_Events", "LOC_w_ECO", "LOC_n_ECO",
-                  "TDO_Executed", "Engaged_OP", "Never_engaged", "Med_Treat_Events",
-                  "Other_events"]
+totals_columns = ["TDOs", "Total_Events"]
 
 def get_totals(doc):
     totals_rows = []
-    year_counter = 2015
+    p_counter = 0
     select_pages = [1, 2, 3, 4, 5, 7, 10, 13]
     doc_pages = list(doc.pages[i] for i in select_pages)
     for page in doc_pages:
-        last_row = page.extract_table()[-1]
-        last_row[0] = year_counter
-        if year_counter < 2017:
-            last_row.insert(1, float("NaN"))
-        year_counter += 1
-        totals_rows.append(last_row)
+        table = page.extract_table()[-13:-2]
+        if p_counter < 2:
+            for row in table:
+                totals_rows.append([row[2], row[4]])
+        else:
+            for row in table:
+                totals_rows.append([row[3], row[5]])
+        p_counter +=1
     return totals_rows
 
 def create_totals_table(rows, cols):
     df = pd.DataFrame(get_totals(pdf), columns=totals_columns)
-    for column in ["ECOs", "Crisis_evals", "TDOs"]:
+    for column in ["TDOs"]:
         df[column] = df[column].str.replace(",", "")
-    for column in ["Per_Total_Evals", "Per_Total_TDOs"]:
-        df[column] = df[column].str.replace("%", "")
     df = df.apply(pd.to_numeric)
     return df
         
 tdos_df = create_totals_table(get_totals(pdf), totals_columns)
 
-x = list(tdos_df["Year"])
-y = list(tdos_df["ECOs"])
-z = list(tdos_df["Total_LOC_Events"])
+x = list(tdos_df["Total_Events"])
 
-plt.plot(x, y)
+plt.plot(x)
 plt.show()
 
 
